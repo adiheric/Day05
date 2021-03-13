b24701f72c39c0db9852b57d039282e7320d4ba8
adiheric <hadiheric@gmail.com>
2021-03-13 05:56:08 +0100

    constructing strings with variables

---
+++ ex06/src/index.js
@@ -0,0 +1,9 @@
+function main() {
+    var myName = "Pikachu";
+    var myString = "Hello, my name is " + myName + " and I am well!";
+  
+    return myString;
+  }
+  
+  console.log(main());
+  module.exports = main;
