4e60e3fa5e1e18d97c20c30b04b82b26ccc75b78
adiheric <hadiheric@gmail.com>
2021-03-13 07:12:57 +0100

    word blanks

---
+++ ex14/src/index.js
@@ -0,0 +1,19 @@
+function main() {
+  var myNoun = "dog";
+  var myVerb = "run";
+  var myAdjective = "big";
+  var myAdverb = "quickly";
+  var wordBlanks =
+    "My " +
+    myNoun +
+    " is " +
+    myVerb +
+    " and " +
+    myAdjective +
+    " " +
+    myAdverb +
+    ".";
+  return wordBlanks;
+}
+console.log(main());
+module.expotrs = main;
